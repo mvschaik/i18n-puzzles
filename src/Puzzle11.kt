a68f19a2ab0d9c68c700256797bcacefbc4db3a8
Maarten van Schaik <maartenvs@google.com>
2025-03-17 14:19:15 +0100

    Completed puzzle 11.

---
+++ src/Puzzle11.kt
@@ -0,0 +1,20 @@
+import java.io.File
+
+
+val ALPHA = "ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ"
+val alpha = "αβγδεζηθικλμνξοπρστυφχψω"
+
+fun caesar(s: String, n: Int) = s.map { c ->
+  when (c) {
+    in ALPHA -> ALPHA[(ALPHA.indexOf(c) + n) % ALPHA.length]
+    in alpha -> alpha[(alpha.indexOf(c) + n) % alpha.length]
+    'ς' -> alpha[(alpha.indexOf('σ') + n) % alpha.length]
+    else -> c
+  }
+}.joinToString("")
+
+fun main(args: Array<String>) {
+  println(File(args[0]).readLines().sumOf { line ->
+    alpha.indices.firstOrNull { "Οδυσσε" in caesar(line, it) } ?: 0
+  })
+}
